2d1c70769a879a76ee4b40a94d0cf9e7dd2f4dcd
jinghangwu <j-wu@kellogg.northwestern.edu>
2015-11-17 20:28:00 -0500

    edit

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ $(function() {
     // Put your code here. Don't change any other code in this file. You will be sad.
       var formatDate=function(time){
               var   now=new Date(parseInt(time) * 1000)
-              var   year=now.getYear()+1900;     
+              var   year=now.getFullYear();     
               var   month=now.getMonth()+1;     
               var   date=now.getDate();     
               var   hour=now.getHours();     
